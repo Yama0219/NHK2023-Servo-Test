ba6f4833a3af7b03a95d814d9700b92ed6abcbdc
Yama0219 <cairnhill.sg1109@gmail.com>
2023-02-16 01:37:18 +0900

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/servo_test.elf.dir/DependInfo.cmake
@@ -0,0 +1,67 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "ASM"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_ASM
+  "C:/Users/kenke/Desktop/Univ/T-semi/NHK2023/STM/servo_test/Core/Startup/startup_stm32g431kbtx.s" "C:/Users/kenke/Desktop/Univ/T-semi/NHK2023/STM/servo_test/cmake-build-debug/CMakeFiles/servo_test.elf.dir/Core/Startup/startup_stm32g431kbtx.s.obj"
+  )
+set(CMAKE_ASM_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_ASM
+  "ARM_MATH_CM4"
+  "ARM_MATH_MATRIX_CHECK"
+  "ARM_MATH_ROUNDING"
+  "DEBUG"
+  "STM32G431xx"
+  "USE_HAL_DRIVER"
+  )
+
+# The include file search paths:
+set(CMAKE_ASM_TARGET_INCLUDE_PATH
+  "C:/Users/kenke/Desktop/Univ/T-semi/NHK2023/STM/servo_test/Core/Inc"
+  "C:/Users/kenke/Desktop/Univ/T-semi/NHK2023/STM/servo_test/Drivers/STM32G4xx_HAL_Driver/Inc"
+  "C:/Users/kenke/Desktop/Univ/T-semi/NHK2023/STM/servo_test/Drivers/STM32G4xx_HAL_Driver/Inc/Legacy"
+  "C:/Users/kenke/Desktop/Univ/T-semi/NHK2023/STM/servo_test/Drivers/CMSIS/Device/ST/STM32G4xx/Include"
+  "C:/Users/kenke/Desktop/Univ/T-semi/NHK2023/STM/servo_test/Drivers/CMSIS/Include"
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/kenke/Desktop/Univ/T-semi/NHK2023/STM/servo_test/Core/Src/main.c" "CMakeFiles/servo_test.elf.dir/Core/Src/main.c.obj" "gcc" "CMakeFiles/servo_test.elf.dir/Core/Src/main.c.obj.d"
+  "C:/Users/kenke/Desktop/Univ/T-semi/NHK2023/STM/servo_test/Core/Src/stm32g4xx_hal_msp.c" "CMakeFiles/servo_test.elf.dir/Core/Src/stm32g4xx_hal_msp.c.obj" "gcc" "CMakeFiles/servo_test.elf.dir/Core/Src/stm32g4xx_hal_msp.c.obj.d"
+  "C:/Users/kenke/Desktop/Univ/T-semi/NHK2023/STM/servo_test/Core/Src/stm32g4xx_hal_timebase_tim.c" "CMakeFiles/servo_test.elf.dir/Core/Src/stm32g4xx_hal_timebase_tim.c.obj" "gcc" "CMakeFiles/servo_test.elf.dir/Core/Src/stm32g4xx_hal_timebase_tim.c.obj.d"
+  "C:/Users/kenke/Desktop/Univ/T-semi/NHK2023/STM/servo_test/Core/Src/stm32g4xx_it.c" "CMakeFiles/servo_test.elf.dir/Core/Src/stm32g4xx_it.c.obj" "gcc" "CMakeFiles/servo_test.elf.dir/Core/Src/stm32g4xx_it.c.obj.d"
+  "C:/Users/kenke/Desktop/Univ/T-semi/NHK2023/STM/servo_test/Core/Src/syscalls.c" "CMakeFiles/servo_test.elf.dir/Core/Src/syscalls.c.obj" "gcc" "CMakeFiles/servo_test.elf.dir/Core/Src/syscalls.c.obj.d"
+  "C:/Users/kenke/Desktop/Univ/T-semi/NHK2023/STM/servo_test/Core/Src/sysmem.c" "CMakeFiles/servo_test.elf.dir/Core/Src/sysmem.c.obj" "gcc" "CMakeFiles/servo_test.elf.dir/Core/Src/sysmem.c.obj.d"
+  "C:/Users/kenke/Desktop/Univ/T-semi/NHK2023/STM/servo_test/Core/Src/system_stm32g4xx.c" "CMakeFiles/servo_test.elf.dir/Core/Src/system_stm32g4xx.c.obj" "gcc" "CMakeFiles/servo_test.elf.dir/Core/Src/system_stm32g4xx.c.obj.d"
+  "C:/Users/kenke/Desktop/Univ/T-semi/NHK2023/STM/servo_test/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal.c" "CMakeFiles/servo_test.elf.dir/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal.c.obj" "gcc" "CMakeFiles/servo_test.elf.dir/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal.c.obj.d"
+  "C:/Users/kenke/Desktop/Univ/T-semi/NHK2023/STM/servo_test/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_cortex.c" "CMakeFiles/servo_test.elf.dir/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_cortex.c.obj" "gcc" "CMakeFiles/servo_test.elf.dir/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_cortex.c.obj.d"
+  "C:/Users/kenke/Desktop/Univ/T-semi/NHK2023/STM/servo_test/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_dma.c" "CMakeFiles/servo_test.elf.dir/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_dma.c.obj" "gcc" "CMakeFiles/servo_test.elf.dir/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_dma.c.obj.d"
+  "C:/Users/kenke/Desktop/Univ/T-semi/NHK2023/STM/servo_test/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_dma_ex.c" "CMakeFiles/servo_test.elf.dir/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_dma_ex.c.obj" "gcc" "CMakeFiles/servo_test.elf.dir/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_dma_ex.c.obj.d"
+  "C:/Users/kenke/Desktop/Univ/T-semi/NHK2023/STM/servo_test/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_exti.c" "CMakeFiles/servo_test.elf.dir/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_exti.c.obj" "gcc" "CMakeFiles/servo_test.elf.dir/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_exti.c.obj.d"
+  "C:/Users/kenke/Desktop/Univ/T-semi/NHK2023/STM/servo_test/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_flash.c" "CMakeFiles/servo_test.elf.dir/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_flash.c.obj" "gcc" "CMakeFiles/servo_test.elf.dir/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_flash.c.obj.d"
+  "C:/Users/kenke/Desktop/Univ/T-semi/NHK2023/STM/servo_test/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_flash_ex.c" "CMakeFiles/servo_test.elf.dir/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_flash_ex.c.obj" "gcc" "CMakeFiles/servo_test.elf.dir/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_flash_ex.c.obj.d"
+  "C:/Users/kenke/Desktop/Univ/T-semi/NHK2023/STM/servo_test/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_flash_ramfunc.c" "CMakeFiles/servo_test.elf.dir/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_flash_ramfunc.c.obj" "gcc" "CMakeFiles/servo_test.elf.dir/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_flash_ramfunc.c.obj.d"
+  "C:/Users/kenke/Desktop/Univ/T-semi/NHK2023/STM/servo_test/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_gpio.c" "CMakeFiles/servo_test.elf.dir/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_gpio.c.obj" "gcc" "CMakeFiles/servo_test.elf.dir/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_gpio.c.obj.d"
+  "C:/Users/kenke/Desktop/Univ/T-semi/NHK2023/STM/servo_test/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_pwr.c" "CMakeFiles/servo_test.elf.dir/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_pwr.c.obj" "gcc" "CMakeFiles/servo_test.elf.dir/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_pwr.c.obj.d"
+  "C:/Users/kenke/Desktop/Univ/T-semi/NHK2023/STM/servo_test/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_pwr_ex.c" "CMakeFiles/servo_test.elf.dir/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_pwr_ex.c.obj" "gcc" "CMakeFiles/servo_test.elf.dir/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_pwr_ex.c.obj.d"
+  "C:/Users/kenke/Desktop/Univ/T-semi/NHK2023/STM/servo_test/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_rcc.c" "CMakeFiles/servo_test.elf.dir/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_rcc.c.obj" "gcc" "CMakeFiles/servo_test.elf.dir/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_rcc.c.obj.d"
+  "C:/Users/kenke/Desktop/Univ/T-semi/NHK2023/STM/servo_test/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_rcc_ex.c" "CMakeFiles/servo_test.elf.dir/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_rcc_ex.c.obj" "gcc" "CMakeFiles/servo_test.elf.dir/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_rcc_ex.c.obj.d"
+  "C:/Users/kenke/Desktop/Univ/T-semi/NHK2023/STM/servo_test/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_tim.c" "CMakeFiles/servo_test.elf.dir/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_tim.c.obj" "gcc" "CMakeFiles/servo_test.elf.dir/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_tim.c.obj.d"
+  "C:/Users/kenke/Desktop/Univ/T-semi/NHK2023/STM/servo_test/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_tim_ex.c" "CMakeFiles/servo_test.elf.dir/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_tim_ex.c.obj" "gcc" "CMakeFiles/servo_test.elf.dir/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_tim_ex.c.obj.d"
+  "C:/Users/kenke/Desktop/Univ/T-semi/NHK2023/STM/servo_test/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_uart.c" "CMakeFiles/servo_test.elf.dir/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_uart.c.obj" "gcc" "CMakeFiles/servo_test.elf.dir/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_uart.c.obj.d"
+  "C:/Users/kenke/Desktop/Univ/T-semi/NHK2023/STM/servo_test/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_uart_ex.c" "CMakeFiles/servo_test.elf.dir/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_uart_ex.c.obj" "gcc" "CMakeFiles/servo_test.elf.dir/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_uart_ex.c.obj.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
